95d87688fe387e9c00160f605851f26928550ff0
Vinicius-Madeira <viniciussmadeira@gmail.com>
2022-11-28 04:03:56 -0300

    Consulta simples com query methods

--- src/main/java/com/viniciusmadeira/workshopmongo/services/PostService.java
+++ src/main/java/com/viniciusmadeira/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.viniciusmadeira.workshopmongo.services.exception.ObjectNotFoundExcept
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> optionalPost = repo.findById(id);
         return optionalPost.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 }
